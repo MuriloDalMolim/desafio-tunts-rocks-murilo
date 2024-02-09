37aa81518be13c627a970a3cb5d642d7ef44a958
murilo <murilodalmolim@gmail.com>
2024-02-09 14:15:14 -0300

    feat: inserted the program logic

--- src/index.js
+++ src/index.js
@@ -17,6 +17,37 @@ import { JWT } from "google-auth-library";
     await sheet.loadHeaderRow(3)
     
     const rows = await sheet.getRows();
-    console.log(rows[3].get('Aluno'));
+    await sheet.loadCells('A1:H27');
+
+    for(var i=0; i< 24 ;i++){
+        var miss = parseInt(rows[i].get('Faltas'));
+
+        if(miss > 15){
+            sheet.getCell(i+3,6).value = "Reprovado por Falta";
+            sheet.getCell(i+3,7).value = 0;
+        }
+        else{
+            var first = parseInt(rows[i].get('P1'));
+            var second = parseInt(rows[i].get('P2'));
+            var third = parseInt(rows[i].get('P3'));
+            var average = Math.ceil((first+second+third)/3).toFixed(0);
+
+            if(average >= 70){
+                sheet.getCell(i+3,6).value ="Aprovado";
+                sheet.getCell(i+3,7).value = 0;
+                
+            }
+            else if(average < 50){
+                sheet.getCell(i+3,6).value ="Reprovado por Nota";
+                sheet.getCell(i+3,7).value = 0;
+            }
+            else{
+                var nfa = 2*70 - average;
+                sheet.getCell(i+3,6).value ="Exame Final";
+                sheet.getCell(i+3,7).value = nfa;
+            }
+        }
+    }
+    await sheet.saveUpdatedCells();
 
 })();
